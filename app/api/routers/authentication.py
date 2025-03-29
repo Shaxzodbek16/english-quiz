660ddefb4a269222d2b81b694e398b48b5c69cb1
= Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-03-29 10:53:13 +0500

    🔀 Merged bot & FastAPI structure 🚀 | Unified project setup & prepared for development 🛠️✅

--- app/api/routers/authentication.py
+++ app/api/routers/authentication.py
@@ -7,4 +7,12 @@ router = APIRouter(
     prefix=settings.API_V1_STR + "/admin/authentication",
     tags=["Admin authentication"],
     redirect_slashes=False,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Authentication credentials were not provided or are invalid."
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": "You do not have permission to access this resource."
+        },
+    },
 )
