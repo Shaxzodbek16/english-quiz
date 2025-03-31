1a49c0c80108f6404bcd9f35704a3245f91a1a62
Jasur <shermatovjasur800@gmail.com>
2025-03-31 11:35:04 +0500

    Google translate web app has been done

--- app/core/middlewares/language.py
+++ app/core/middlewares/language.py
@@ -5,3 +5,8 @@ from aiogram.utils.i18n.middleware import I18nMiddleware as I18nMiddlewareBase
 
 class I18nMiddleware(I18nMiddlewareBase):  # noqa
     pass
+
+
+_ = I18nMiddlewareBase.gettext
+
+messages.answer(_(""))
