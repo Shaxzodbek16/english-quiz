441921d53204830f5ccf7a525ad6c4fc285928a5
Jasur <shermatovjasur800@gmail.com>
2025-04-01 08:59:14 +0500

    main buttons is created and connected postgres

--- app/core/middlewares/language.py
+++ app/core/middlewares/language.py
@@ -5,8 +5,3 @@ from aiogram.utils.i18n.middleware import I18nMiddleware as I18nMiddlewareBase
 
 class I18nMiddleware(I18nMiddlewareBase):  # noqa
     pass
-
-
-_ = I18nMiddlewareBase.gettext
-
-messages.answer(_(""))
